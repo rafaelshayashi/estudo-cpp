af9b5de3d7f5b889f028c8d025372d12895bcdf6
Rafael Simionato Hayashi <rafael.hayashi@net.com.br>
2019-08-26 15:04:35 -0300

    Retirado binários do commit

--- char/const.cpp
+++ char/const.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
-
 static const char arrayDeChar[] = "Array de Char";
 static const char* ponteiroParaChar[] = {"Ponteiro para char"};
 
 int recebePonteiro(const char *ponteiroParaChar[]){
 
     std::cout << "O array de char é na funcao é:" << &ponteiroParaChar << std::endl;
-
-    std::cout << "O array de char é na funcao é:" << ponteiroParaChar[0] << std::endl;
+    std::cout << "O ponteiro para array de char é na funcao é:" << ponteiroParaChar[0] << std::endl;
 
 }
 
@@ -16,7 +14,7 @@ int main(){
 
 
     std::cout << "O array de char é :" << arrayDeChar << std::endl;
-    std::cout << "O array de char é :" << &ponteiroParaChar << std::endl;
+    std::cout << "O ponteiro array de char é :" << &ponteiroParaChar << std::endl;
 
     recebePonteiro(ponteiroParaChar);
 
